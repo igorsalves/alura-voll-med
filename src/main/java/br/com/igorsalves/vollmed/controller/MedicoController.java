a70c18cc4da6f5810a3a7e6fcab116de3a127b12
Igor Silva Alves~ <igorsaj@hotmail.com>
2023-11-05 16:44:51 -0300

    feat: definido padrão da paginação

--- src/main/java/br/com/igorsalves/vollmed/controller/MedicoController.java
+++ src/main/java/br/com/igorsalves/vollmed/controller/MedicoController.java
@@ -9,6 +9,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
@@ -38,7 +39,7 @@ public class MedicoController {
   }
 
   @GetMapping
-  public ResponseEntity<Page<DadosListagemMedico>> listar(Pageable pageable) {
+  public ResponseEntity<Page<DadosListagemMedico>> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable pageable) {
     var medicos = medicoRepository.findAll(pageable).map(DadosListagemMedico::new);
 
     return ResponseEntity.status(HttpStatus.OK).body(medicos);
